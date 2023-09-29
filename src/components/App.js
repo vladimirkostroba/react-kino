d559688532c3133b4d1272a9ffc4bb772381a1f7
Vladimir Kostroba <vladimirkostroba22@gmail.com>
2023-09-29 06:35:09 +0200

    commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import Home from "pages/Home";
+import SearchMovies from "pages/SearchMovies";
+import SharedLayout from "./SharedLayout";
+
+function App(){
+    return(
+        <Routes>
+         <Route path="/" element={<SharedLayout/>}>
+           <Route index element={<Home/>}/>
+           <Route element={<SearchMovies/>}/>
+         </Route>
+        </Routes>
+   )
+}
+
+export default App
