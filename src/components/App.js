30971d70119889ee496ff05fc32f7d91be05e3b4
Vladimir Kostroba <vladimirkostroba22@gmail.com>
2023-09-29 06:49:06 +0200

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ function App(){
         <Routes>
          <Route path="/" element={<SharedLayout/>}>
            <Route index element={<Home/>}/>
-           <Route element={<SearchMovies/>}/>
+           <Route path="/searchMovies" element={<SearchMovies/>}/>
          </Route>
         </Routes>
    )
--- src/pages/SearchMovies.js
+++ src/pages/SearchMovies.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function SearchMovies(){
     return(
-        <p>HEY</p>
+        <p>mbnvcbs</p>
     )
 }
