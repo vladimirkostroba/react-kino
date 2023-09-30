3fecf9919ff8f2a36816d3b5b96b56971142c52f
Vladimir Kostroba <vladimirkostroba22@gmail.com>
2023-09-30 13:53:20 +0200

    добавил api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,38 @@
-import React from "react";
+import  { useEffect,useState } from "react";
+
+const moviesApi = (exportedData,isLoading) => {
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMjMwYmJjZGE0MWM2OWYwYmZmZGE1ZDNkNjRlYmNkNSIsInN1YiI6IjY1MDdjM2QxNDJkOGE1MDExYmQ3NDAxNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Oxa75X3JVqtzgOHh5yPUmGQIUF4IZjqi2OkYakrysPk'
+        }
+      };
+      
+      return fetch('https://api.themoviedb.org/3/trending/all/day?language=en-US', options)
+        .then(response => response.json())
+        .then(({results}) => {
+            exportedData(results)
+            isLoading(false)})
+        .catch(err => console.error(err));
+    
+}
 
 export default function Home(){
-    return(
-        <p>HEY</p>
-    )
+
+    const [movies, setMovies] = useState();
+    const [isLoading, setIsLoading] = useState(true);
+
+
+    useEffect(() => {
+        moviesApi(setMovies,setIsLoading);
+    },[isLoading])
+
+    return{
+        
+    }
 }
+
+// https://ru.stackoverflow.com/questions/1511283/react-fetch-%D0%B8-useeffect-%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80-%D0%BA%D0%BE%D0%BC%D0%BF%D0%BE%D0%BD%D0%B5%D0%BD%D1%82%D0%B0-%D0%BD%D0%B5%D1%81%D0%BA%D0%BE%D0%BB%D1%8C%D0%BA%D0%BE-%D1%80%D0%B0%D0%B7
+
+// сделать список фильмов в отдельном компоненте в компоненте
